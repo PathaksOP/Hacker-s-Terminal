24834b159851f3598c6e3abb70d21c93229da5bd
PathaksOP <namitapathak1983@gmail.com>
2026-05-06 21:03:21 +0530

    added base code

---
+++ script.js
@@ -0,0 +1,43 @@
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const showLine = async () => {
+  for (let i = 0; i < 5; i++) {
+    randomMS = Math.floor(Math.random() * (7000 - 1000 + 1) + 1000);
+    let div = document.querySelector(`#d${i + 1}`);
+    await delay(randomMS);
+    div.classList.remove("hidden");
+    animateDots(div);
+  }
+};
+
+const animateDots = (element) => {
+  let d1 = document.createElement("p");
+  d1.classList.add("hidden");
+  d1.innerText = ".";
+  let d2 = document.createElement("p");
+  d2.classList.add("hidden");
+  d2.innerText = ".";
+  let d3 = document.createElement("p");
+  d3.classList.add("hidden");
+  d3.innerText = ".";
+  element.append(d1, d2, d3);
+  let dots = [d1, d2, d3];
+  setInterval(() => {
+    dots.forEach((dot, index) => {
+      setTimeout(
+        () => {
+          dot.classList.remove("hidden");
+        },
+        500 * (index + 1),
+      );
+      setTimeout(
+        () => {
+          dot.classList.add("hidden");
+        },
+        500 * (index + 2),
+      );
+    });
+  }, 3000);
+};
+
+showLine();
